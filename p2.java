371f0746bbbad1ba59285d9aac62fba47f0f20b7
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-12-12 20:34:07 +0530

    files added

---
+++ p2.java
@@ -0,0 +1,20 @@
+public class Application{
+    public static void main(String[] args)
+    {
+        //8 primitive datatypes in java
+        int myNumber=88;
+        //integer
+        byte myByte=22;
+        //8 bits data
+        short st=43;
+        //64 bits
+        long lg=2333;
+        //128 bits
+        float fl=3.4;
+        //decimal 32 bits
+        double db=444.4;
+        //decimal 64 bits
+        System.out.printIn(myNumber);
+        //declaration of variable
+    }
+}
