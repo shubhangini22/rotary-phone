1b7776a30e4ffbb3036be649e24f509e63e4bd96
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-12-13 18:25:00 +0530

    files added

---
+++ p5.java
@@ -0,0 +1,8 @@
+public class p5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("HEllo");
+            // for loops
+        }
+    }
+}
