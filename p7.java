8f392488a33d918c04d2415c427529c59a96792d
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-12-15 17:44:05 +0530

    files added

--- p7.java
+++ p7.java
@@ -1,5 +1,10 @@
-public class p7 {
-     public static void main(String[] args) {
-        
+import java.util.Scanner;
+class HelloWorld {
+    public static void main(String[] args) {
+       Scanner myObj= new Scanner(System.in);
+       System.out.println("Give input");
+       String input=myObj.nextLine();
+       System.out.println("Input is"+" "+input);
+       //output
     }
 }
---
+++ p8.java
@@ -0,0 +1,12 @@
+class App {
+    public static void main(String[] args) {
+        // declaration
+        int arr[];
+        arr = new int[8];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println(arr[0]);
+        System.out.println(arr.length);
+        // loop through the array using for loop
+    }
+}
