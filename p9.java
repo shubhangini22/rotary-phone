67d84f871bbe4110d6fccc3d7687a5de9aa7be3c
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2022-12-16 20:28:27 +0530

    Create p9.java

---
+++ p9.java
@@ -0,0 +1,12 @@
+class Person{
+    String name;
+    int age;
+}
+class HelloWorld {
+    public static void main(String[] args) {
+        Person person1=new Person();
+        person1.name="Ram";
+        person1.age=37;
+        System.out.println(person1.name);
+    }
+}
