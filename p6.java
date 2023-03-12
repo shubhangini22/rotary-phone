e9f310d42fdf3c3eb214c9f23409ecd7557af72f
shubhangini22 <88343647+shubhangini22@users.noreply.github.com>
2023-03-12 12:25:16 +0530

    Update p6.java

--- p6.java
+++ p6.java
@@ -1,3 +1,14 @@
 public class p6 {
-    
+     public static void main(String[] args) {
+        boolean b=true;
+   
+   if(b==false)
+   {
+       System.out.println("hello");
+   }
+   else
+   {
+       System.out.println("hello1");
+   }
+    }
 }
